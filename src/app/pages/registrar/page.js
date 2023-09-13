0a24e794125ad71cb542444dd77a16b5e067d659
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-13 11:10:12 -0400

    registrar

---
+++ src/app/pages/registrar/page.js
@@ -0,0 +1,25 @@
+'use client'
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+const Formulario = () => { 
+  const handlerLogin = async (e) => {
+    e.preventDefault();
+    toast.success('Registrado com sucesso!!!!!!!!!!')
+  }
+  return (
+    <div class="body">
+      <form onSubmit={handlerLogin}>
+      <h1>Entrar</h1>
+      <input placeholder='Nome' type="nome" ></input>
+        <input placeholder='E-mail' type="email"></input>
+        <input placeholder='Senha' type='password'> </input>
+        <button>Entrar</span></button>
+        <button><a href="/pages/dashboard">Voltar</a></button>
+      </form>
+      <ToastContainer/>
+    </div>
+  )
+};
+
+export default Formulario;
