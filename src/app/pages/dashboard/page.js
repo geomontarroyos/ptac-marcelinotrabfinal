019b75fa50f8d86349843069e9e10266cc0df08c
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-13 09:47:08 -0400

    Update page.js

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,10 +1,16 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
+import { Suspense } from "react";
+import Listar from "@/app/componentes/listar";
 
 export default async function Dashboard() {
-   
+    const users = getUsers()
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div> 
+            <Suspense fallback={<p> carregando pagina...</p>}>
+                <Listar users={users}/>
+                <button><a href="/pages/alterar">Alterar</a></button>
+                <button><a href="/pages/registrar">Registrar</a></button>
+</Suspense>
             
         </div>
     );
