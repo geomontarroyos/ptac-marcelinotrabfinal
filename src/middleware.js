47c615aed7c3445054c51afc82542f589d675f74
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-09-13 10:26:35 -0400

    verificando se um usuário está autenticado antes de permitir o acesso e configurando as rotas

--- src/middleware.js
+++ src/middleware.js
@@ -6,16 +6,24 @@ export const middleware = (request) => {
 
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
+    const Users = new URL('./pages/dashboard' , request.url);
     const isTokenValidated = validateToken(token);
 
     if (!isTokenValidated || !token) {
-        if (request.nextUrl.pathname === '/pages/dashboard') {
+        if (request.nextUrl.pathname === './pages/dashboard'|| 
+        request.nextUrl.pathname === '/pages/alterar' || request.nextUrl.pathname === '/pages/registrar'
+        )  {
             return NextResponse.redirect(urlLogin);
+        }
+    }
+    if (isTokenValidated) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect(Users);
         }
     }
     NextResponse.next();
 };
 export const config = {
-    matcher: ['/', '/pages/dashboard']
+    matcher: ['/', '/pages/dashboard', '/pages/alterar', '/pages/registrar', ]
 };
 
