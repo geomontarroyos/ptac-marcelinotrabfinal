b06b384161404b0f08010b26d79e5dab2299116e
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-11-14 09:05:50 -0400

    criacao postUser (para enviar os dados) e criacao da const userSave que vai salvar os dados que foi enviado retornar esses dados

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -8,9 +8,8 @@ const responsepOfApi = await fetch(url + "/user/authenticated",
     method:"POST",
     headers:{"content-Type": "application/json"},
     body: JSON.stringify(userLogin)
-}
+});
 
-);
 const userAuth = await responsepOfApi.json();
 console.log(userAuth)
 return userAuth;
@@ -28,4 +27,21 @@ const getUsers = async () =>{
     }
 }
 
-export { getUsers, getUserAuthenticated };
+const postUser = async (user) => {
+try{
+    const responsepOfApi = await fetch(url + "/user", 
+    {
+        method:"POST",
+        headers:{"content-Type": "application/json"},
+        body: JSON.stringify(userLogin)
+    });
+
+const userSave = await responsepOfApi.json();
+return userSave;
+}catch(error){
+    console.error(`Error: ${error}`);
+    return null;
+}
+}
+
+export { getUsers, getUserAuthenticated, postUser  };
