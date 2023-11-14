4e3fd63792d6cf0d906cee988b1ff006686337fe
marisantosteixeira <mariana.teixeira2@estudante.ifms.edu.br>
2023-11-14 09:44:53 -0400

    decodificar o token

--- src/app/functions/validateToken.js
+++ src/app/functions/validateToken.js
@@ -4,8 +4,12 @@ import {decode} from "jsonwebtoken";
 const validateToken = async (token)=>{
 const secret = "hfdsfbewufwefqkjfQHI"
 try{
-    const isTokenValidate = await jwtVerify(token,
+    /*const isTokenValidate = await jwtVerify(token,
          new TextEncoder().encode(secret));
+         if(isTokenValidate){
+            return true
+         }*/
+             const isTokenValidate = await decode(token)
          if(isTokenValidate){
             return true
          }
